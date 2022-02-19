f4360f16cffd973db95f0e61a7a194f13dc85cbc
sanjivsahani <DK0520@digikull.com>
2022-02-19 16:06:42 +0530

    axios

--- src/context/ArrState.jsx
+++ src/context/ArrState.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React,{useState,useEffect} from 'react'
 import ArrContext from './ArrContext'
 
 const ArrState = (props) => {
-    let arr = [1,2,3,4,5]
-    //https://api.thecatapi.com/v1/images/search
+    const [cat ,setcat]=useState()
+    const [dog ,setdog]=useState()
+
+useEffect(() => {
+  
+    axios.get("https://api.thecatapi.com/v1/images/search").then((res)=>{
+        setcat(res)
+    })
+  
+}, [cat])
+useEffect(() => {
+  
+    axios.get("https://dog.ceo/api/breeds/image/random").then((res)=>{
+        setdog(res)
+    })
+  
+}, [dog])
+    
+
+    
+    // https://api.thecatapi.com/v1/images/search
     //https://dog.ceo/api/breeds/image/random
 
   return (
-    <ArrContext.Provider value={{arr}}>
+    <ArrContext.Provider value={{ cat,dog}}>
         {props.children}
     </ArrContext.Provider>
   )
